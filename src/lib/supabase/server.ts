ba61551da72cf33736b6d74eb42d913e5afce019
Ra U <raunaqopp@gmail.com>
2025-07-14 10:28:32 +0000

    errorss

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -5,10 +5,10 @@ import { cookies } from 'next/headers';
 export async function createServer(useServiceRoleKey = false) {
   const cookieStore = cookies();
 
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+  const supabaseUrl = process.env.SUPABASE_URL!;
   const supabaseKey = useServiceRoleKey 
     ? process.env.SUPABASE_SERVICE_ROLE_KEY! 
-    : process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+    : process.env.SUPABASE_ANON_KEY!;
 
   if (useServiceRoleKey && !process.env.SUPABASE_SERVICE_ROLE_KEY) {
       console.error("SUPABASE_SERVICE_ROLE_KEY is not set. This is required for admin actions like deleting users.");
