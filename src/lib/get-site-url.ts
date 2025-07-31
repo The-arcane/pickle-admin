c78a9620e46e3b7f9da88c51f9e4d3e2c1c6e870
Ra U <raunaqopp@gmail.com>
2025-07-31 12:53:46 +0000

    Still redirecting to apt

--- src/lib/get-site-url.ts
+++ src/lib/get-site-url.ts
@@ -1,14 +1,14 @@
 
 export const getSiteURL = () => {
-    let url =
-      process.env.NEXT_PUBLIC_SITE_URL ?? // Set this to your custom domain in .env.local
-      process.env.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel
-      process.env.URL ??                    // Automatically set by Netlify
-      'http://localhost:9002';
-      
-    // Make sure to include `https://` when not localhost.
-    url = url.includes('http') ? url : `https://${url}`;
-    // Make sure to include a trailing `/`.
-    url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
-    return url;
+  let url =
+    process.env.NEXT_PUBLIC_SITE_URL || // Set this to your custom domain in .env.local
+    process.env.NEXT_PUBLIC_VERCEL_URL || // Automatically set by Vercel
+    process.env.URL ||                    // Automatically set by Netlify
+    'http://localhost:9002';
+    
+  // Make sure to include `https://` when not localhost.
+  url = url.includes('http') ? url : `https://${url}`;
+  // Make sure to include a trailing `/`.
+  url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
+  return url;
 };
