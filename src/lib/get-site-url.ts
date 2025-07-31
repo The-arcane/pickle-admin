ea84e587d266eb69da1639e64701b4abb198f6cb
Ra U <raunaqopp@gmail.com>
2025-07-31 12:51:04 +0000

    Now again inam getting logged in to production directly instead of Netli

--- src/lib/get-site-url.ts
+++ src/lib/get-site-url.ts
@@ -1,9 +1,9 @@
 
 export const getSiteURL = () => {
     let url =
-      process.env.NEXT_PUBLIC_SITE_URL ?? // Set this to your local URL in .env.local
-      process.env.NEXT_PUBLIC_VERCEL_URL ?? // Vercel deployment URL
-      process.env.URL ??                    // Netlify deployment URL
+      process.env.NEXT_PUBLIC_SITE_URL ?? // Set this to your custom domain in .env.local
+      process.env.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel
+      process.env.URL ??                    // Automatically set by Netlify
       'http://localhost:9002';
       
     // Make sure to include `https://` when not localhost.
