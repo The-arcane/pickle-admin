ea2e620f9b67c6e551e8e58b831d032b331752af
Dipin E <dipine@gmail.com>
2025-08-20 08:00:51 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <CardContent>
    
    wit

--- src/app/login/client.tsx
+++ src/app/login/client.tsx
@@ -35,7 +35,7 @@ export function LoginForm() {
           </CardTitle>
           <CardDescription>Enter your credentials to access your dashboard.</CardDescription>
         </CardHeader>
-        <CardContent>
+        <CardContent className="pt-0">
           {error && (
             <Alert variant="destructive" className="mb-4">
               <AlertCircle className="h-4 w-4" />
