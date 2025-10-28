c1a4eff474d1f57b2760c96e129a52add7bcee3d
Dipin E <dipine@gmail.com>
2025-10-28 10:10:51 +0000

    Version 16.3.19 to Version 16.3.31

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -122,7 +122,7 @@ export default function DashboardLayout({
           </div>
           <div className="mt-auto p-4">
               <p className="text-xs text-muted-foreground capitalize">
-                {organisationType && `${organisationType} | `}Version 16.3.19
+                {organisationType && `${organisationType} | `}Version 16.3.31
               </p>
           </div>
         </aside>
