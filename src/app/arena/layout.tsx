570cf9a17ed717c7fa6e2118d7c65c637ea09254
Dipin E <dipine@gmail.com>
2025-11-04 06:41:29 +0000

    Version 16.3.96 to Version 16.3.99

--- src/app/arena/layout.tsx
+++ src/app/arena/layout.tsx
@@ -112,7 +112,7 @@ export default function ArenaLayout({
             </div>
             <div className="mt-auto p-4">
                 <p className="text-xs text-muted-foreground capitalize">
-                  {organisationType && `${organisationType} | `}Version 16.3.96
+                  {organisationType && `${organisationType} | `}Version 16.3.99
                 </p>
             </div>
           </aside>
@@ -140,7 +140,7 @@ export default function ArenaLayout({
                     </div>
                      <div className="mt-auto p-4">
                         <p className="text-xs text-muted-foreground capitalize">
-                          {organisationType && `${organisationType} | `}Version 16.3.96
+                          {organisationType && `${organisationType} | `}Version 16.3.99
                         </p>
                     </div>
                 </SheetContent>
--- src/app/login/client.tsx
+++ src/app/login/client.tsx
@@ -78,7 +78,7 @@ export function LoginForm() {
             </div>
             <SubmitButton />
           </form>
-          <p className="mt-4 text-center text-xs text-muted-foreground">Version 16.3.96</p>
+          <p className="mt-4 text-center text-xs text-muted-foreground">Version 16.3.99</p>
         </CardContent>
       </Card>
     </div>
