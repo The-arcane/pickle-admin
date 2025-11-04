e323d0decbfce4b7cd5c71ef36668f771aaafa6e
Dipin E <dipine@gmail.com>
2025-11-04 09:03:32 +0000

    Version 16.3.99 to Version 16.6.3

--- src/app/arena/layout.tsx
+++ src/app/arena/layout.tsx
@@ -112,7 +112,7 @@ export default function ArenaLayout({
             </div>
             <div className="mt-auto p-4">
                 <p className="text-xs text-muted-foreground capitalize">
-                  {organisationType && `${organisationType} | `}Version 16.3.99
+                  {organisationType && `${organisationType} | `}Version 16.6.3
                 </p>
             </div>
           </aside>
@@ -140,7 +140,7 @@ export default function ArenaLayout({
                     </div>
                      <div className="mt-auto p-4">
                         <p className="text-xs text-muted-foreground capitalize">
-                          {organisationType && `${organisationType} | `}Version 16.3.99
+                          {organisationType && `${organisationType} | `}Version 16.6.3
                         </p>
                     </div>
                 </SheetContent>
